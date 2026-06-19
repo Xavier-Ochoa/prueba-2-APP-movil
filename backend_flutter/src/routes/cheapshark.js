e283b89b70b4631cbfef69f0ae12da646fe63294
Xavier <luis.ochoa02@epn.edu.ec>
2026-06-19 11:19:01 -0500

    Delete backend_flutter directory

--- backend_flutter/src/routes/cheapshark.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDeals, getDealById } = require('../controllers/cheapsharkController');
-
-router.get('/deals', getDeals);
-router.get('/deals/:dealId', getDealById);
-
-module.exports = router;
